e466e2fa4d9b28e9d05f6215ba490dcf409c0179
adam wojciechowski <adi61044@gmail.com>
2019-10-16 18:03:38 +0200

    main cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        FileReader test = new FileReader("test_instance.txt");
-        test.importInstance();
-        System.out.println(test.getFilePath());
-        test.setFileName("nowy");
-        System.out.println(test.getFilePath());
+        System.out.println("hello salesman!!!");
     }
 }
